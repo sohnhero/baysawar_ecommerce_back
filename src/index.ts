25e32657b2db95c4ba8a50ac0692c854775d6973
Ndiaga Lo <ndiagalo259@gmail.com>
2026-03-25 08:47:30 +0000

    few changes

--- src/index.ts
+++ src/index.ts
@@ -21,7 +21,7 @@ const port = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://shop.fabiratrading.com',
   credentials: true
 }));
 app.use(express.json());
